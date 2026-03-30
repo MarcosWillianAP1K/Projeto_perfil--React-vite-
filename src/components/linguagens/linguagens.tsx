f1cdc7ed8ba434267a1387c42fea0bcd8ef4c9b4
MarcosWillianAP1K <mwss@outlook.com>
2026-03-30 15:16:20 -0300

    Componente linguagem

--- src/components/linguagens/linguagens.tsx
+++ src/components/linguagens/linguagens.tsx
@@ -5,7 +5,9 @@ function Linguagens(){
 
 
     return(
-        <div className="p-4 w-60 h-100 bg-gray-100 flex items-start justify-content rounded-2xl">
+        <div className="p-4 w-60 h-100 space-y-2 bg-gray-100 flex-col items-start justify-content rounded-2xl">
+
+            <h1 className="text-lg font-bold">Linguagens</h1>
             
             <div className="space-x-4 space-y-2 flex-col ">
                 <Perfil_linguagens directory="src/assets/foto_perfil.png" string="Lindooooooooooooo" />
